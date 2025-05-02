267da66c1777517d5814eae6159a5e1f9cbab807
gao yang <3038780655@qq.com>
2025-05-02 18:07:18 +0100

    update main page

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -1,31 +1,92 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3n from "aws-cdk-lib/aws-s3-notifications";
-import * as events from "aws-cdk-lib/aws-lambda-event-sources";
-import * as sqs from "aws-cdk-lib/aws-sqs";
-import * as sns from "aws-cdk-lib/aws-sns";
-import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
-import * as iam from "aws-cdk-lib/aws-iam";
-
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { Duration, RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const imagesBucket = new s3.Bucket(this, "images", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    const Bucket = new s3.Bucket(this, "bucket", {
+      removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       publicReadAccess: false,
     });
 
-    // Output
-    
+    const DLQ = new sqs.Queue(this, "record-dlq", {
+      retentionPeriod: Duration.days(1),
+    });
+
+    const imageQueue = new sqs.Queue(this, "record-queue", {
+      visibilityTimeout: Duration.seconds(30),
+      deadLetterQueue: {
+        queue: DLQ,
+        maxReceiveCount: 1,
+      },
+    });
+
+    const imagesTable = new dynamodb.Table(this, "imagestable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    const recordImageFn = new lambdanode.NodejsFunction(this, "recordImageFn", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      entry: `${__dirname}/../lambdas/processImage.ts`,
+      timeout: Duration.seconds(10),
+      memorySize: 128,
+      environment: {
+        TABLE_NAME: imagesTable.tableName,
+      },
+    });
+
+    Bucket.grantRead(recordImageFn);
+    imagesTable.grantWriteData(recordImageFn);
+
+    Bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED,
+      new s3n.SqsDestination(imageQueue)
+    );
+
+    recordImageFn.addEventSource(
+      new SqsEventSource(imageQueue, {
+        batchSize: 1,
+        maxBatchingWindow: Duration.seconds(5),
+      })
+    );
+
+    const deleteInvalidFn = new lambdanode.NodejsFunction(this, "deleteInvalidFn", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+      timeout: Duration.seconds(10),
+      memorySize: 128,
+    });
+
+    Bucket.grantDelete(deleteInvalidFn);
+
+    deleteInvalidFn.addEventSource(
+      new SqsEventSource(DLQ, {
+        batchSize: 1,
+        maxBatchingWindow: Duration.seconds(5),
+      })
+    );
+
+   
+
+
     new cdk.CfnOutput(this, "bucketName", {
-      value: imagesBucket.bucketName,
+      value: Bucket.bucketName,
+    });
+
+    new cdk.CfnOutput(this, "tableName", {
+      value: imagesTable.tableName,
     });
   }
 }
